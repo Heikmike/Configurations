4b482727b5aebbfa3b5e445c1d88010790c1d333
Heikel Jebali <heikel.jebali@epfl.ch>
2023-05-22 09:17:50 +0200

    Update

--- hypr/scripts/battery.sh
+++ hypr/scripts/battery.sh
@@ -3,16 +3,25 @@
 # Minimum battery percentage to trigger the notification
 MIN_BATTERY_PERCENTAGE=20
 
-while true; do
-    # Get the current battery percentage
-    BATTERY_PERCENTAGE=$(acpi -b | grep -P -o '[0-9]+(?=%)')
+is_charger_connected() {
+    acpi -a | grep -q "on-line"
+    return $?
+}
 
-    # Check if the battery percentage is below the threshold
-    if [[ $BATTERY_PERCENTAGE -lt $MIN_BATTERY_PERCENTAGE ]]; then
-        # Send a notification using Dunst
-        dunstify --urgency=critical "Battery Low" "Battery is at ${BATTERY_PERCENTAGE}%."
-    fi
+main () {
+  while true; do
+      # Get the current battery percentage
+      BATTERY_PERCENTAGE=$(acpi -b | grep -P -o '[0-9]+(?=%)')
 
-    # Sleep for 5 minutes before checking again
-    sleep 180
-done
+      # Check if the battery percentage is below the threshold
+      if [[ $BATTERY_PERCENTAGE -lt $MIN_BATTERY_PERCENTAGE ]] && ! is_charger_connected; then
+          # Send a notification using Dunst
+          dunstify --urgency=critical "Battery Low" "Battery is at ${BATTERY_PERCENTAGE}%."
+      fi
+
+      # Sleep for 5 minutes before checking again
+      sleep 180
+  done
+}
+
+main
