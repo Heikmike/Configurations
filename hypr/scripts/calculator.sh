5577487ffd43334a809f8ce498b30c34325996cd
Heikel Jebali <heikel.jebali@epfl.ch>
2023-10-03 18:15:11 +0200

    Update

--- hypr/scripts/calculator.sh
+++ hypr/scripts/calculator.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-kitty --class calculator --title calculator --name calculator --single-instance --hold -- bash -c "bc -l; read -p 'Press enter to close'" &
+kitty --single-instance --hold \
+  -- bash -c "bc -l -q" &
 
 sleep 0.1
 hyprctl dispatch togglefloating
 sleep 0.1
 hyprctl dispatch resizeactive exact 800 500
-# sleep 0.5
-# hyprctl dispatch centerwindow
-# sleep 0.5
-# hyprctl dispatch moveactive exact 500 -300
-
