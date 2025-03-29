53497bcfd7251e51efce29b2f7508b5b0fa063e9
Heikel Jebali <heikel.jebali@epfl.ch>
2025-03-29 13:44:28 +0100

    Working sleep script

--- hypr/scripts/sleep.sh
+++ hypr/scripts/sleep.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
+current_brightness=$(brightnessctl get)
+
 swayidle -w timeout 300 'swaylock -f' \
-            timeout 330 'hyprctl dispatch dpms off' \
-            resume 'hyprctl dispatch dpms on' \
+            timeout 330 'brightnessctl set 0' \
+            resume "brightnessctl set $current_brightness" \
             timeout 390 'systemctl suspend' \
-            before-sleep 'swaylock -f' \
-            after-resume 'hyprctl dispatch dpms on' &
+            before-sleep 'swaylock -f' &
