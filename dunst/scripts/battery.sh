c89da409a8a2096fea0656ff491406e90e096d0e
Heikel Jebali <heikel.jebali@epfl.ch>
2025-04-21 22:09:56 +0200

    Fix battery scripts

--- dunst/scripts/battery.sh
+++ dunst/scripts/battery.sh
@@ -4,11 +4,20 @@ icon="/home/heikmike/Documents/Repositories/Configurations/dunst/icons/battery_l
 appname="Battery"
 title="Battery Low"
 timeout=10000
+test="0"
 
 while true
 do
     # Get battery value and check if charging
-    battery=$(acpi -b | grep -P -o '[0-9]+(?=%)' | head -n 1)
+    first_battery=$(acpi -b | grep -P -o '[0-9]+(?=%)' | head -n 1)
+    second_battery=$(acpi -b | grep -P -o '[0-9]+(?=%)' | head -n 2 | tail -1)
+
+    if [[ $first_battery == "0" ]]; then
+      battery=$second_battery
+    else
+      battery=$first_battery
+    fi
+
     is_charging=$(acpi -b | grep -P -o 'Charging')
     message="${battery}% left"
 
--- dunst/scripts/batteryCheck.sh
+++ dunst/scripts/batteryCheck.sh
@@ -6,7 +6,15 @@ title="Battery State"
 timeout=3000
 
 # Get battery value and check if charging
-battery=$(acpi -b | grep -P -o '[0-9]+(?=%)' | head -n 1)
+first_battery=$(acpi -b | grep -P -o '[0-9]+(?=%)' | head -n 1)
+second_battery=$(acpi -b | grep -P -o '[0-9]+(?=%)' | head -n 2 | tail -1)
+
+if [[ $first_battery == "0" ]]; then
+  battery=$second_battery
+else
+  battery=$first_battery
+fi
+
 message="${battery}% left"
 dunstify -I $icon -a $appname -t $timeout $title $message
 
